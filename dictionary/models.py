838a54327d2cf76ff547f42cb7de0cfe5e92359d
Vincent Malic <vqmalic@gmail.com>
2026-07-10 15:20:30 +0500

    finishing up models; source temporary for now

--- dictionary/models.py
+++ dictionary/models.py
@@ -15,19 +15,23 @@ class PartOfSpeech(models.Model):
 	name = models.CharField("POS Full Name", max_length=200)	# e.g. noun, verb, adjective
 	description = models.TextField("POS Description")			# the usual, although useful for very specific Turkic grammar explanations
 
-class DefinitionSource(models.Model):
-	pass
+class Source(models.Model):
+	# this is just temporary, saving more complicated stuff for later
+	year = models.IntegerField("Year of Publication")
+	shortname = models.CharField("Source Short Name", max_length=200) # Just for the UI, so people can identify the source
+	identifier = models.CharField("Source Identifier", max_length=1000) # Unique identifiers, placeholder field, think ISBN, or NBN
 
 class Definition(models.Model):
 	entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
 	pos = models.ForeignKey(PartOfSpeech, on_delete=models.CASCADE)
 	definition_text = models.TextField("Definition Text")									# e.g. "booty, plunder, spoils of war"
-	definition_source = models.ForeignKey(DefinitionSource, on_delete=models.CASCADE)		
+	definition_source = models.ForeignKey(Source, on_delete=models.CASCADE)		
 
 class UsageExample(models.Model):
 	definition = models.ForeignKey(Definition, on_delete=models.CASCADE) 
 	persoarabic_usage = models.TextField("Usage Example in Perso-Arabic")
 	latinstrict_usage = models.TextField("Usage Example in Verbatim Latin")
+	usage_source = models.ForeignKey(Source, on_delete=models.CASCADE)
 
 class Language(models.Model):
 	name = models.CharField("Language Name", max_length=200)		# e.g. Arabic, Old Turkic, Persian
