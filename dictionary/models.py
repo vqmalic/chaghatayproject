1aad75a871be231b201886dfbe2cdd77c0f2a2d8
Vincent Malic <vqmalic@gmail.com>
2026-07-10 15:49:45 +0500

    minor corrections

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Entry(models.Model):
-	headword = models.CharField("Perso-Arabic Headword", max_length=500)				# e.g. غنیمت
+	persoarabic = models.CharField("Perso-Arabic Headword", max_length=500)				# e.g. غنیمت
 	latin_strict = models.CharField("Verbatim Latin Transliteration", max_length=500)	# e.g. ġnymt
 	latin_phonetic = models.CharField("Phonetic Latin Transcription", max_length=500)	# e.g. ġanīmat
 
 class AlternateSpelling(models.Model):
 	entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
-	persoarabic_alt = models.CharField("Alternate Spelling in Perso-Arabic", max_length=500)
-	latinstrict_alt = models.CharField("Alternate Spelling in Verbatim Latin", max_length=500)
+	persoarabic = models.CharField("Alternate Spelling in Perso-Arabic", max_length=500)
+	latin_strict = models.CharField("Alternate Spelling in Verbatim Latin", max_length=500)
 
 class PartOfSpeech(models.Model):
 	abb = models.CharField("POS Abbreviation", max_length=50)	# e.g. N., V., Adj.
@@ -23,21 +23,24 @@ class Source(models.Model):
 
 class Definition(models.Model):
 	entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
-	pos = models.ForeignKey(PartOfSpeech, on_delete=models.CASCADE)
+	pos = models.ForeignKey(PartOfSpeech, on_delete=models.PROTECT)
 	definition_text = models.TextField("Definition Text")									# e.g. "booty, plunder, spoils of war"
-	definition_source = models.ForeignKey(Source, on_delete=models.CASCADE)		
+	source = models.ForeignKey(Source, on_delete=models.PROTECT, null=True, blank=True)		# optional, because some definitions are not sourced	
 
 class UsageExample(models.Model):
 	definition = models.ForeignKey(Definition, on_delete=models.CASCADE) 
-	persoarabic_usage = models.TextField("Usage Example in Perso-Arabic")
-	latinstrict_usage = models.TextField("Usage Example in Verbatim Latin")
-	usage_source = models.ForeignKey(Source, on_delete=models.CASCADE)
+	persoarabic = models.TextField("Usage Example in Perso-Arabic")
+	latin_strict = models.TextField("Usage Example in Verbatim Latin")
+	gloss = models.TextField("English Gloss of Usage Example", blank=True)
+	source = models.ForeignKey(Source, on_delete=models.PROTECT)
 
 class Language(models.Model):
 	name = models.CharField("Language Name", max_length=200)		# e.g. Arabic, Old Turkic, Persian
 	iso_code = models.CharField("ISO 639-3 Code", max_length=3)		# e.g. ara, ota, pes
 
 class Etymology(models.Model):
-	language = models.ForeignKey(Language, on_delete=models.CASCADE)	# e.g. from Arabic
+	# possibly implement sequence to record chronology
+	entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+	language = models.ForeignKey(Language, on_delete=models.PROTECT)	# e.g. from Arabic
 	word = models.CharField("Source Word", max_length=500)				# e.g. غَنِيمَة
 
