c73c8fe70dec2074ba57d35daf6a29d5017cb7e8
Vincent Malic <vqmalic@gmail.com>
2026-08-03 18:54:13 +0500

    file location bugs

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Entry
 
-# Create your views here.
+class EntryListView(ListView):
+    model = Entry
+    template_name = 'dictionary/entry_list.html'
+    context_object_name = 'entries'
+    paginate_by = 50
+    ordering = ['latin_strict_stripped']
