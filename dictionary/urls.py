258a5f7c14f4062a62a456726b37caf1b1c22e66
Vincent Malic <vqmalic@gmail.com>
2026-08-03 18:57:53 +0500

    initial work entry view

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EntryListView
+from .views import EntryListView, EntryDetailView
 
 urlpatterns = [
     path('', EntryListView.as_view(), name='entry_list'),
+    path('entry/<int:pk>/', EntryDetailView.as_view(), name='entry_detail'),
 ]
--- dictionary/views.py
+++ dictionary/views.py
@@ -7,3 +7,8 @@ class EntryListView(ListView):
     context_object_name = 'entries'
     paginate_by = 50
     ordering = ['latin_strict_stripped']
+
+class EntryDetailView(DetailView):
+    model = Entry
+    template_name = 'dictionary/entry_detail.html'
+    context_object_name = 'entry'
